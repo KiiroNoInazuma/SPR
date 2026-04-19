781b6c77670edb97de8a5ba6c82adc33656d564a
John Rey <adioskid@mail.ru>
2026-04-20 02:08:27 +0400

    #3

---
+++ greeting-app/src/test/java/ru/app/work/greetingapp/GreetingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.app.work.greetingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
