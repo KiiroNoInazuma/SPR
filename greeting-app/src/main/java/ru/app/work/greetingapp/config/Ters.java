d297ae61a8fb2f081852c94e9718b9b80c492b58
John Rey <adioskid@mail.ru>
2026-04-25 00:12:32 +0400

    #6

---
+++ greeting-app/src/main/java/ru/app/work/greetingapp/config/Ters.java
@@ -0,0 +1,14 @@
+package ru.app.work.greetingapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Ters {
+
+    @Bean
+    public void test(){
+        System.out.println("aaaaaaaaaaa");
+    }
+
+}
--- io-starter/src/main/java/ru/app/work/iostarter/IOAutoconfiguration.java
+++ io-starter/src/main/java/ru/app/work/iostarter/IOAutoconfiguration.java
@@ -1,10 +1,17 @@
 package ru.app.work.iostarter;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.app.work.iostarter.config.DefaultLocaleProvider;
+import ru.app.work.iostarter.config.LocaleProvider;
 import ru.app.work.iostarter.service.IOService;
+import ru.app.work.iostarter.service.LocalizationService;
+import ru.app.work.iostarter.service.LocalizationServiceImpl;
 import ru.app.work.iostarter.service.StreamsIOService;
 
 @ConditionalOnProperty(value = "ru.app.work.io-enabled", havingValue = "true", matchIfMissing = true)
@@ -15,5 +22,20 @@ public class IOAutoconfiguration {
     @Bean
     public IOService ioService() {
         return new StreamsIOService(System.out, System.in);
+    }
+
+    @ConditionalOnMissingBean(LocaleProvider.class)
+    @Bean
+    public LocaleProvider localeProvider(@Value("${ru.app.work.locale:ru-RU}") String localeTag) {
+        return new DefaultLocaleProvider(localeTag);
+
+    }
+
+    @ConditionalOnMissingBean(LocalizationService.class)
+    @ConditionalOnBean(MessageSource.class)
+    @Bean
+    public LocalizationService localizationService(LocaleProvider localeProvider,
+                                                   MessageSource messageSource) {
+        return new LocalizationServiceImpl(localeProvider, messageSource);
     }
 }
